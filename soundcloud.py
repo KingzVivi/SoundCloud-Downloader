87cf5867ed8d58989f8be8b24279e01138d92453
DaRealFreak <steffen.keuper@web.de>
2013-08-08 20:50:17 +0200

    Update soundcloud.py

--- soundcloud.py
+++ soundcloud.py
@@ -33,9 +33,31 @@ class SoundCloud(object):
 	def __reporthook(self, blocks_read, block_size, total_size):
 		# clean the system line and 'updating' our status
 		sys.stdout.write("\r% 3.1f%% of %d bytes "
-			% (float(blocks_read * block_size) / total_size * 100, total_size))
+			% (float(blocks_read * block_size) / total_size * 100, self._humanize_bytes(total_size)))
 		sys.stdout.flush()
 
+	def _humanize_bytes(self, bytes, precision=1):
+		"""
+		Return a humanized string representation of a number of bytes.
+		Assumes `from __future__ import division`.
+		copied from here: http://code.activestate.com/recipes/577081-humanized-representation-of-a-number-of-bytes/
+		"""
+		
+		abbrevs = (
+			(1<<50L, 'PB'),
+			(1<<40L, 'TB'),
+			(1<<30L, 'GB'),
+			(1<<20L, 'MB'),
+			(1<<10L, 'kB'),
+			(1, 'bytes')
+		)
+		if bytes == 1:
+			return '1 byte'
+		for factor, suffix in abbrevs:
+			if bytes >= factor:
+				break
+		return '%.*f %s' % (precision, bytes / factor, suffix)
+		
 	def _download_file(self, data):
 		# parsing the data dictionary to retrieve the streaming url and create a valid file path
 		stream_url = data['stream_url']
